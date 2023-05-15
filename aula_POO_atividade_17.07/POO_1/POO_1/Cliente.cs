4b20492d95b7259ecb604cd050e4d6133a52bb90
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2023-05-15 17:38:57 -0300

    aula_POO_atividade_17.07

---
+++ aula_POO_atividade_17.07/POO_1/POO_1/Cliente.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_1
+{
+    class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string endereço { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+
+        public Cliente(int id, string nome, string endereço, string telefone, string email)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.endereço = endereço;
+            this.telefone = telefone;
+            this.email = email;
+        }
+
+        public Cliente()
+        {
+        }
+
+
+        public static void add()
+        {
+            Console.Write("Digite o seu nome: ");
+            string name = Console.ReadLine();
+            Console.Write("Digite o seu endereço: ");
+            string address = Console.ReadLine();
+            Console.Write("Digite o seu telefone: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Digite o seu email: ");
+            string eMail = Console.ReadLine();
+
+            string client_details = name + "," + address + "," + phoneNumber + "," + eMail;
+            create_line(client_details);
+        }
+
+        public void deleteById()
+        {
+
+        }
+
+        public void searchById()
+        {
+
+        }
+
+        public void listAll()
+        {
+
+        }
+
+        public void editById()
+        {
+
+        }
+
+        public static void create_line(string client_detail)
+        {
+            string path = "C:/Users/8054703/OneDrive/POO_1/POO_1/teste.csv";
+            if (File.Exists(path))
+            {
+                string teste = File.ReadAllText(path);
+                if (teste == "")
+                {
+                    File.AppendAllText(path, client_detail);
+                }
+                else
+                {
+                    File.AppendAllText(path, '\n' + client_detail);
+                }
+            }
+        }
+
+        public static void readAll()
+        {
+            string path = "C:/Users/8054703/OneDrive/POO_1/POO_1/teste.csv";
+            if (File.Exists(path))
+            {
+                string teste = File.ReadAllText(path);
+                Console.WriteLine(File.ReadAllText(path));
+                Console.Read();
+            }
+        }
+
+    }
+}
---
+++ aula_POO_atividade_17.07/POO_1/POO_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static POO_1.Cliente;
+
+namespace POO_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int escolha = 0;
+
+            while(escolha != 5)
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Digite ->");
+                    Console.WriteLine("1 para cadastrar cliente");
+                    Console.WriteLine("2 para editar cliente");
+                    Console.WriteLine("3 para exluir cliente");
+                    Console.WriteLine("4 para listar cliente");
+                    Console.Write("Escolha: ");
+                }
+                while (!int.TryParse(Console.ReadLine(), out escolha));
+
+                if (escolha == 1)
+                {
+                    Cliente.add();
+                }else if(escolha == 4)
+                {
+                    Cliente.readAll();
+                }
+
+            }
+        }
+        
+    }
+}
