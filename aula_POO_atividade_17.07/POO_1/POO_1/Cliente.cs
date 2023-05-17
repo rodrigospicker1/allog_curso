328c4a861f525e2fe93ca92904a0c53a9f45f8e0
rodrigospicker1 <r.spicker13@gmail.com>
2023-05-17 15:24:10 -0300

    project_done

--- aula_POO_atividade_17.07/POO_1/POO_1/Cliente.cs
+++ aula_POO_atividade_17.07/POO_1/POO_1/Cliente.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POO_1
-{
-    class Cliente
+﻿class Cliente
     {
         public int id { get; set; }
         public string nome { get; set; }
@@ -28,9 +19,28 @@ namespace POO_1
         {
         }
 
+        public static bool verifyEmail(string eMail){
+            if(eMail == ""){
+                return false;
+            }
+            int teste = eMail.IndexOf("@");
+            if(teste != -1){
+                string[] dados = eMail.Split('@');
+                string[] depoisAroba = dados[1].Split('.');
+                if(depoisAroba[0] != "" && depoisAroba[1] != ""){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
 
         public static void add()
         {
+            Console.Clear();
+            Console.WriteLine("Adicione um novo usuário");
             Console.Write("Digite o seu nome: ");
             string name = Console.ReadLine();
             Console.Write("Digite o seu endereço: ");
@@ -39,34 +49,113 @@ namespace POO_1
             string phoneNumber = Console.ReadLine();
             Console.Write("Digite o seu email: ");
             string eMail = Console.ReadLine();
+            while(verifyEmail(eMail) != true){
+                Console.Write("Email inválido, digite o1 email denovo: ");
+                eMail = Console.ReadLine();
+            }
 
-            string client_details = name + "," + address + "," + phoneNumber + "," + eMail;
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
+            int id = 1;
+            var lines = File.ReadAllLines(path);
+            var all = File.ReadAllText(path);
+            if(all != ""){
+                var last_line = lines.Last();
+                var dados = last_line.Split(',');
+                id = int.Parse(dados[0])+1;
+            }
+
+            string client_details = id + "," + name + "," + address + "," + phoneNumber + "," + eMail;
             create_line(client_details);
+            Console.Clear();
+            Console.Write("Usuário inserido com sucesso\n");
         }
 
-        public void deleteById()
+        public static void deleteById()
         {
+            Console.Write("Qual id você deseja deletar: ");
+            string id = Console.ReadLine();
 
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
+            var lines = File.ReadLines(path);
+            var Result = new List<string>();
+            foreach (var line in lines){
+                string[] dados = line.Split(',');
+                if(dados[0] != id){
+                    Result.Add(line);                        
+                }
+            }
+            File.WriteAllLines(path, Result);
+
+            Console.Clear();
+            Console.Write("Usuário deletado com sucesso\n");
         }
 
-        public void searchById()
+        public static void searchById()
         {
-
+            Console.Write("Qual id você deseja ver: ");
+            string id = Console.ReadLine();
+            Console.Clear();
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
+            var lines = File.ReadLines(path);
+            foreach (var line in lines){
+                string[] dados = line.Split(',');
+                if(dados[0] == id){  
+                    Console.WriteLine("Usuário encontrado"); 
+                    Console.WriteLine("Nome: "+dados[1]); 
+                    Console.WriteLine("Endereço: "+dados[2]);  
+                    Console.WriteLine("Telefone: "+dados[3]); 
+                    Console.WriteLine("Email: "+dados[4]+"\n");   
+                    return;          
+                }
+            }
+            Console.WriteLine("Usuário encontrado\n");
         }
 
-        public void listAll()
+        public static void listAll()
         {
-
+            readAll();
         }
 
-        public void editById()
+        public static void editById()
         {
+            Console.Write("Qual id você deseja ver: ");
+            string id = Console.ReadLine();
+            Console.Clear();
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
+            var lines = File.ReadLines(path);
+            var Result = new List<string>();
+            foreach (var line in lines){
+                string[] dados = line.Split(',');
+                if(dados[0] != id){
+                    Result.Add(line);                        
+                }else{
+                    Console.Write("Usuário encontrado\n");
+                    Console.Write("Digite o novo nome: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Digite o novo endereço: ");  
+                    string address = Console.ReadLine();
+                    Console.Write("Digite o novo telefone: ");
+                    string phoneNumber = Console.ReadLine();
+                    Console.Write("Digite o novo email: ");
+                    string eMail = Console.ReadLine();
+                    while(verifyEmail(eMail)){
+                      Console.Write("Email inválido, digite o novo email denovo: ");
+                    eMail = Console.ReadLine();
+                    }
 
+                    string client_details = line[0] + "," + name + "," + address + "," + phoneNumber + "," + eMail;
+                    Result.Add(client_details);
+                    
+                }
+            }
+            File.WriteAllLines(path, Result);
+            Console.Clear();
+            Console.Write("Usuário atualizado com sucesso\n");
         }
 
         public static void create_line(string client_detail)
         {
-            string path = "C:/Users/8054703/OneDrive/POO_1/POO_1/teste.csv";
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
             if (File.Exists(path))
             {
                 string teste = File.ReadAllText(path);
@@ -83,14 +172,19 @@ namespace POO_1
 
         public static void readAll()
         {
-            string path = "C:/Users/8054703/OneDrive/POO_1/POO_1/teste.csv";
+            string path = "C:/Users/sysadmin/Desktop/Projetos/.NET/allog_curso-main/allog_curso-main/Teste.net/teste.csv";
             if (File.Exists(path))
             {
-                string teste = File.ReadAllText(path);
-                Console.WriteLine(File.ReadAllText(path));
-                Console.Read();
+                Console.Clear();
+                var lines = File.ReadLines(path);
+                foreach (var line in lines){
+                    string[] dados = line.Split(',');
+                        Console.WriteLine("\nNome: "+dados[1]); 
+                        Console.WriteLine("Endereço: "+dados[2]);  
+                        Console.WriteLine("Telefone: "+dados[3]); 
+                        Console.WriteLine("Email: "+dados[4]+"\n");      
+            }
             }
         }
 
     }
-}
--- aula_POO_atividade_17.07/POO_1/POO_1/Program.cs
+++ aula_POO_atividade_17.07/POO_1/POO_1/Program.cs
@@ -1,42 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static POO_1.Cliente;
-
-namespace POO_1
-{
-    class Program
+﻿class Program
     {
         static void Main()
         {
             int escolha = 0;
+            Console.Clear();
 
-            while(escolha != 5)
+            while(escolha != 6)
             {
                 do
                 {
-                    Console.Clear();
                     Console.WriteLine("Digite ->");
                     Console.WriteLine("1 para cadastrar cliente");
                     Console.WriteLine("2 para editar cliente");
                     Console.WriteLine("3 para exluir cliente");
                     Console.WriteLine("4 para listar cliente");
+                    Console.WriteLine("5 para listar todos cliente");
+                    Console.WriteLine("6 para SAIR");
                     Console.Write("Escolha: ");
                 }
                 while (!int.TryParse(Console.ReadLine(), out escolha));
 
-                if (escolha == 1)
-                {
+                if (escolha == 1){
                     Cliente.add();
-                }else if(escolha == 4)
-                {
-                    Cliente.readAll();
+                }else if(escolha == 2){
+                    Cliente.editById();
+                }else if(escolha == 3){
+                    Cliente.deleteById();
+                }else if(escolha == 4){
+                    Cliente.searchById();
+                }else if(escolha == 5){
+                    Cliente.listAll();
+                }else{
+                    Console.Clear();
                 }
 
             }
         }
         
     }
-}
